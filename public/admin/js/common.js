3d0335e3dec7e000ce05a25ab99243659221a75b
jiangning <13127637801@163.com>
2017-11-30 18:38:25 +0800

    第一次提交

---
+++ public/admin/js/common.js
@@ -0,0 +1,29 @@
+//关闭进度环
+// NProgress.configure({
+//   showSpinner: false
+// });
+
+// $(document).ajaxStart(function () {
+//   //开始进度条
+//   NProgress.start();
+// });
+
+// $(document).ajaxStop(function () {
+//   //结束进度条
+//   setTimeout(function () {
+//     NProgress.done();
+//   }, 500);
+// });
+
+
+//二级菜单显示与隐藏效果
+$(".child").prev().on("click", function () {
+  $(this).next().slideToggle();
+});
+
+
+//侧边栏显示与隐藏效果
+$(".icon_menu").on("click", function () {
+  $(".lt_aside").toggleClass("now");
+  $(".lt_main").toggleClass("now");
+});
